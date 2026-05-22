e957bb64e23a99977cd4d6d1391261cf4caf34f0
Glen Egbert <glenegbert1@gmail.com>
2026-05-22 09:41:17 -0600

    Like/unlike photos with Turbo Streams (#14)
    * implement like funtionality
    
    * add routes
    
    * remove set_photo and add a validation
    
    * address security issue with link
    
    * update regex

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,21 @@
+class LikesController < ApplicationController
+  def create
+    photo = Photo.find(params[:photo_id])
+    @like = photo.likes.find_or_create_by(user: current_user)
+    @photo = Photo.with_likes_count.find(params[:photo_id])
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to photos_path }
+    end
+  end
+
+  def destroy
+    current_user.likes.find_by(id: params[:id])&.destroy
+    @like = nil
+    @photo = Photo.with_likes_count.find(params[:photo_id])
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to photos_path }
+    end
+  end
+end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
     @photos = Photo.with_likes_count
-    @liked_photo_ids = current_user.liked_photo_ids
+    @likes_by_photo_id = current_user.likes.index_by(&:photo_id)
   end
 end
